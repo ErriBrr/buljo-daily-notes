efd2d33a90aaee09d03b43cead7a0ff3b22b9b24
ErriBrr <rer-b@laposte.net>
2021-11-24 10:24:44 +0100

    [FEATURE] Import Button : Fixes #11

--- src/app/components/toolbar-buttons/toolbar-buttons.component.ts
+++ src/app/components/toolbar-buttons/toolbar-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NoteService } from '../../services/note.service';
 
 @Component({
@@ -7,8 +7,15 @@ import { NoteService } from '../../services/note.service';
   styleUrls: ['./toolbar-buttons.component.css']
 })
 export class ToolbarButtonsComponent implements OnInit {
+  @ViewChild('fileInput')
+  fileInput: any;
+  
   exportCsvToFile(): void {
     this.noteService.exportNotes();
+  }
+
+  importCsvToFile(): void {
+    this.fileInput.nativeElement.click();
   }
 
   importFile(e: any) {
